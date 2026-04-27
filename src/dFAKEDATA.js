d7f7a0fff6541a9e7266edd55604ceda8be3c5c4
brmerah-create <br.merah@ensta.edu.dz>
2026-04-27 19:30:33 +0100

    Create dFAKEDATA.js with mock patient and doctor data
    Added mock data for patients, doctors, and appointments.

---
+++ src/dFAKEDATA.js
@@ -0,0 +1,26 @@
+export const patients = [
+  {
+    id: 1,
+    first_name: "Ali",
+    last_name: "Benali",
+    region: "Bejaia ",
+    phone: "055769045",
+    age: 22,
+    blood: "A+",
+    problem: "Flu",
+  },
+];
+
+export const doctors = [
+  { id: 1, name: "Dr Ahmed", specialization: "Cardiology", fee: 2000 },
+  { id: 2, name: "Dr Maria", specialization: "Dermatology", fee: 2500 },
+];
+
+export const appointments = [
+  {
+    id: 958559941,
+    doctor: "Dr Ahmed",
+    date: "2026-05-01",
+    status: "Scheduled",
+  },
+];
