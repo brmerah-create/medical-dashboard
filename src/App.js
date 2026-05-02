8e033019b5bbde52deefe5f6ec3073747214d936
brmerah-create <br.merah@ensta.edu.dz>
2026-05-02 12:11:17 +0100

    Implement data fetching for patients, doctors, and appointments
    Fetch patient, doctor, and appointment data from API on component mount.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import {
-  patients as initialPatients,
-  doctors as initialDoctors,
-  appointments as initialAppointments,
-} from "./data/FAKEDATA";
+import { useState, useEffect } from "react";
 
 import PatientSection from "./components/PatientCard";
 import AppointmentSection from "./components/AppointmentCard";
@@ -12,23 +7,43 @@ import DoctorSection from "./components/DoctorCard";
 import "./App.css";
 
 export default function App() {
-  const [patients, setPatients] = useState(initialPatients);
-  const [doctors, setDoctors] = useState(initialDoctors);
-  const [appointments, setAppointments] = useState(initialAppointments);
+  const [patients, setPatients] = useState([]);
+  const [doctors, setDoctors] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+
+  
+  useEffect(() => {
+    
+    fetch("http://127.0.0.1:8000/api/patients/")
+      .then(res => res.json())
+      .then(data => setPatients(data))
+      .catch(err => console.error("Patients error:", err));
+
+    
+    fetch("http://127.0.0.1:8000/api/doctors/")
+      .then(res => res.json())
+      .then(data => setDoctors(data))
+      .catch(err => console.error("Doctors error:", err));
+
+    
+    fetch("http://127.0.0.1:8000/api/appointments/")
+      .then(res => res.json())
+      .then(data => setAppointments(data))
+      .catch(err => console.error("Appointments error:", err));
+  }, []);
 
   return (
     <div className="app">
       <h1>🏥 Medical Dashboard</h1>
-    
+
       <PatientSection patients={patients} setPatients={setPatients} />
-    
+
       <AppointmentSection
         doctors={doctors}
         appointments={appointments}
         setAppointments={setAppointments}
       />
 
-     
       <DoctorSection doctors={doctors} setDoctors={setDoctors} />
     </div>
   );
